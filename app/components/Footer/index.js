937629a832495f49b146fb78b68b9dd896e2ebd3
Anh <ltanh2710@gmail.com>
2020-01-10 18:15:19 +0700

    Footer

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-
-import A from 'components/A';
-import LocaleToggle from 'containers/LocaleToggle';
-import Wrapper from './Wrapper';
-import messages from './messages';
 import './Footer.css';
 import imageFooter from '../../asset/Image/Group_106.jpg';
 import logo_blue from '../../asset/Icon/Logo_blue.svg';
+import IC_MAIL from '../../asset/Icon/IC_MAIL.svg';
+import GROUP_96 from '../../asset/Icon/Group96.svg';
+import IC_PHONE from '../../asset/Icon/ic_phone.svg';
 import ic_phone from '../../asset/Icon/ic_phone.svg';
 import ic_location from '../../asset/Icon/ic_location.svg';
 import ic_mail from '../../asset/Icon/ic_mail.svg';
@@ -18,10 +15,47 @@ import flag_vn from '../../asset/Image/flag_vn.jpg';
 
 function Footer() {
   return (
-    <Wrapper>
-      <section className="a">
-        <img src={imageFooter} alt="Snow" style={{ width: '100%' }} />
-        <div
+    <div className='footer-container'>
+      <section className='section-top'>
+        <img src={imageFooter} alt="Snow" style={{ width: '100%' }}/>
+
+        <img src={logo_blue} alt="Snow" className="logo-bmv"/>
+
+        <span className='company-name-vi'>{'CÔNG TY CỔ PHẦN QUẢN LÝ TÒA NHÀ VIỆT NAM'}</span>
+        <span className='company-name-en'>{'Vietnam Building Management.,JSC'}</span>
+
+        <div className='contact-container'>
+          <div className='contact__item'>
+            <div className='contact__icon'>
+              <img src={IC_MAIL} alt=""/>
+            </div>
+            <span className='contact__text'>{'bm-v@gmail.com'}</span>
+          </div>
+          <div className='contact__item'>
+            <div className='contact__icon'>
+              <img src={GROUP_96} alt=""/>
+            </div>
+            <span className='contact__text'>{'Tầng 5 toà A1 toà nhà Ecolife Capital, số 58 Tố Hữu, Nam Từ Liêm, HN'}</span>
+          </div>
+          <div className='contact__item'>
+            <div className='contact__icon'>
+              <img src={IC_PHONE} alt=""/>
+            </div>
+            <span className='contact__text'>{'+84 24 666666 84'}</span>
+          </div>
+        </div>
+
+        <div className='language'>
+          <div className='language__title'>{'Ngôn ngữ'}</div>
+          <div className='language__item clearfix'>
+            <img className='language__flag' src={flag_vn} />
+            <img className='language__flag' src={flag_en} />
+            <img className='language__flag' src={flag_japan} />
+          </div>
+
+        </div>
+
+        {/*<div
           style={{
             position: 'absolute',
             top: 0,
@@ -41,49 +75,24 @@ function Footer() {
             }}
           >
             <div>
-              <img src={logo_blue} alt="Snow" className="logo_blue" />
+              <img src={logo_blue} alt="Snow" className="logo_blue"/>
               <div>CÔNG TY CỔ PHẦN QUẢN LÝ TÒA NHÀ VIỆT NAM</div>
               <div className="centered_1">Vietnam Building Management.,JSC</div>
             </div>
-            <div
-              style={{
-                width: '100%',
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-around',
-              }}
-            >
-              <div>
-                <img src={ic_mail} alt="ic_mail" />
-                <div style={{ color: '#FFFFFF' }}>bm-v@gmail.com</div>
-              </div>
-              <div>
-                <img src={ic_location} alt="ic_location" />
-                <div style={{ color: '#FFFFFF' }}>
-                  Tầng 5 toà A1 toà nhà Ecolife Capital
-                </div>
-                <div style={{ color: '#FFFFFF' }}>
-                  số 58 Tố Hữu, Nam Từ Liêm, HN
-                </div>
-              </div>
-              <div>
-                <img src={ic_phone} alt="ic_phone" />
-                <div style={{ color: '#FFFFFF' }}>+84 24 666666 84</div>
-              </div>
-            </div>
+
             <div>
               <div style={{ color: '#FFFFFF' }}>Ngôn ngữ</div>
-              <img src={flag_vn} alt="flag_vn" />
-              <img src={flag_en} alt="flag_en" style={{ padding: '0 10px' }} />
-              <img src={flag_japan} alt="flag_japan" />
+              <img src={flag_vn} alt="flag_vn"/>
+              <img src={flag_en} alt="flag_en" style={{ padding: '0 10px' }}/>
+              <img src={flag_japan} alt="flag_japan"/>
             </div>
           </div>
-        </div>
+        </div>*/}
       </section>
       <section className="copyright">
-        <div className="b">Copyright © 2020 bm-v. All rights reserved.</div>
+        <div className="copyright__content">Copyright © 2020 bm-v. All rights reserved.</div>
       </section>
-    </Wrapper>
+    </div>
   );
 }
 
--- app/containers/HomePage/NoiDungChinh.js
+++ app/containers/HomePage/NoiDungChinh.js
@@ -1,18 +1,4 @@
 import React, { Fragment } from 'react';
-import hinh_vuong from '../../asset/Image/hinh_vuong.jpg';
-import hinh_nha_1 from '../../asset/Image/hinh_nha_1.png';
-import logo_blue from '../../asset/Icon/Logo_blue.svg';
-import ic_mail from '../../asset/Icon/ic_mail.svg';
-import ic_location from '../../asset/Icon/ic_location.svg';
-import ic_phone from '../../asset/Icon/ic_phone.svg';
-import flag_vn from '../../asset/Image/flag_vn.jpg';
-import flag_en from '../../asset/Image/flag_en.jpg';
-import flag_japan from '../../asset/Image/flag_japan.jpg';
-
-import ic_cleaner from '../../asset/Icon/ic_cleaner.svg';
-import ic_insecticides from '../../asset/Icon/ic_insecticides.svg';
-import ic_management from '../../asset/Icon/ic_management.svg';
-import ic_security from '../../asset/Icon/ic_security.svg';
 import HINH_VUONG from '../../asset/Image/hinh_vuong.jpg';
 import TIANSHU_LIU from '../../asset/Image/tianshu-liu.png';
 import MASK_GROUP_16 from '../../asset/Image/MaskGroup16.png';
@@ -99,7 +85,8 @@ export function NoiDungChinh() {
         </div>
         <div className='content__item'>
           <div className='content__item__number'>{'500'}</div>
-          <div className='content__item__text'>{'Hơn 500 cán bộ công nhân viên với tay nghề nhiệt huyết'}</div>
+          <div className='content__item__text'>{'Hơn 500 cán bộ công nhân viên'}</div>
+          <div className='content__item__text'>{'với tay nghề nhiệt huyết'}</div>
         </div>
         <div className='content__item'>
           <div className='content__item__number'>{'7'}</div>
@@ -108,107 +95,6 @@ export function NoiDungChinh() {
 
         </div>
       </div>
-
-
-      {/*<div
-        style={{
-          position: 'absolute',
-          top: '625px',
-          width: '100%',
-          height: '524px',
-          left: 0,
-          backgroundImage: `url(${nha_kinh})`,
-        }}
-      >
-        <div
-          style={{ display: 'flex', flexDirection: 'column', height: '100%' }}
-        >
-          <div
-            style={{ width: '100%', display: 'flex', flexDirection: 'row' }}
-          >
-            <div>
-              <img src={ic_insecticides} alt="ic_location"/>{' '}
-              <span>Diệt côn trùng</span>
-              <div style={{ color: '#FFFFFF' }}>
-                Kiểm soát phòng ngừa côn trùng là một thành phần thiết yếu của
-                quá trình đang bảo trì tài sản, và một đều không may là thường
-                xuyên bị bỏ qua.
-              </div>
-            </div>
-            <div>
-              <img src={ic_management} alt="ic_phone"/>{' '}
-              <span>Quản lý vận hành</span>
-              <div style={{ color: '#FFFFFF' }}>
-                Chúng tôi luôn nỗ lực phát huy công năng tối đa thông qua việc
-                quản lý vận hành chính xác, ổn định các thiết bị phòng cháy,
-                chữa cháy, thiết bị điều hòa.
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ width: '100%', display: 'flex', flexDirection: 'row' }}
-          >
-            <div>
-              <img src={ic_security} alt="ic_location"/>{' '}
-              <span>Bảo vệ, camera an ninh</span>
-              <div style={{ color: '#FFFFFF' }}>
-                Chúng tôi luôn luôn thực hiện nghiêm khắc để bảo toàn tài sản
-                thông qua việc giám sát bên trong, bên ngoài tòa nhà bằng hệ
-                thống camera an ninh.
-              </div>
-            </div>
-            <div>
-              <img src={ic_cleaner} alt="ic_phone"/>{' '}
-              <span>Vệ sinh công nghiệp</span>
-              <div style={{ color: '#FFFFFF' }}>
-                Để duy trì vệ sinh, chăm sóc sàn nhà, duy trì mỹ quan bên
-                trong tòa nhà thì có công việc tiến hành hàng ngày và có công
-                việc tiến hành định kỳ.
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>*/}
-
-      {/*<img
-        src={nha_kinh_2}
-        alt="hinh_nha_1"
-        style={{
-          position: 'absolute',
-          top: '337px',
-          width: '251px',
-          right: 0,
-        }}
-      />*/}
-      {/*<div
-        style={{
-          position: 'absolute',
-          bottom: 0,
-          width: '100%',
-          height: '178px',
-          background: '#CE001B',
-        }}
-      >
-        <div
-          style={{ display: 'flex', flexDirection: 'row', height: '100%' }}
-        >
-          <div>
-            <div>50</div>
-            <div style={{ color: '#FFFFFF' }}>Trên 50 năm kinh nghiệm</div>
-          </div>
-          <div>
-            <div>500</div>
-            <div style={{ color: '#FFFFFF' }}>
-              Hơn 500 cán bộ công nhân viên <br/>
-              với tay nghề nhiệt huyết
-            </div>
-          </div>
-          <div>
-            <div>7</div>
-            <div style={{ color: '#FFFFFF' }}>7 lĩnh vực chủ chốt</div>
-          </div>
-        </div>
-      </div>*/}
     </div>
   );
 }
